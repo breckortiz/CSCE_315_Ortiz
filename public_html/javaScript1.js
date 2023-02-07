083a1e57e3db421505562fb7f543c1d52043c978
breckortiz <breck.ortiz12@gmail.com>
2023-02-06 19:45:45 -0600

    test
    test

--- public_html/javaScript1.js
+++ public_html/javaScript1.js
@@ -20,3 +20,73 @@ function openTab(event, tabName) {
 
     event.currentTarget.className += " active";
   }
+
+  function OldtoggleButton() {
+    var theme = document.getElementsByTagName('link')[0];
+    if (theme.getAttribute('href') == 'StyleSheet1.css') {
+        theme.setAttribute('href', 'StyleSheet2.css');
+    } else {
+        theme.setAttribute('href', 'StyleSheet1.css');
+    }
+
+    page_type = localStorage.getItem('page_type');
+
+
+    if(page_type === null){
+   	  page_type = 2;
+      localStorage.setItem("page_type", 2);
+    }
+
+    if(page_type == 1){
+      StyleSheet_1_mode();
+    }
+    else{
+      StyleSheet_2_mode();
+    }
+}
+
+function toggleButton() {
+
+  page_type = localStorage.getItem('page_type');
+
+
+  if(page_type === null){
+     page_type = 2;
+    localStorage.setItem("page_type", 2);
+  }
+
+  if(page_type == 1){
+    StyleSheet_1_mode();
+  }
+  else{
+    StyleSheet_2_mode();
+  }
+}
+
+function load_style()
+{
+    page_style = localStorage.getItem('page_stylesheet_name');
+    if(page_style === null){
+   	 page_style = "StyleSheet2.css";
+    }
+    document.getElementById('page_style').setAttribute("href", page_style);
+}
+
+function StyleSheet_2_mode()
+{
+    localStorage.setItem("page_stylesheet_name", "StyleSheet2.css");
+    load_style();
+}
+
+function StyleSheet_1_mode()
+{
+    localStorage.setItem("page_stylesheet_name", "StyleSheet1.css");
+    load_style();
+}
+
+load_style();  // load style immediately
+
+
+
+
+
